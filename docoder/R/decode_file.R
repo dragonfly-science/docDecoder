e47636124a642f75961c99776ac8eefc7a6a8aa6
Edward Abraham <edward@dragonfly.co.nz>
2014-12-12 14:07:03 +1300

    Update decode_file.R

--- docoder/R/decode_file.R
+++ docoder/R/decode_file.R
@@ -1,4 +1,4 @@
 decode_file <-
-function(file, debug = FALSE, test = TRUE, summary = TRUE) {
+function(file, debug = FALSE, test = FALSE, summary = TRUE) {
     return(decode_string(readLines(file, warn=F), debug=debug, test=test, summary=summary))
 }
